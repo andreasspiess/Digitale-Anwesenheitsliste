72adf6f96cc39710747f4c3ea776c009a85f9815
Your Name <you@example.com>
2022-03-31 10:03:35 +0200

    Initial commit

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -1,4 +1,3 @@
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -6,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String getStart() {
+    public String getStartpage() {
 
         return "startpage";
     }
