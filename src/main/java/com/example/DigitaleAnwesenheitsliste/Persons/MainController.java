bf4b04e7be42543135552a4f6cc61f2eb1f255f7
Your Name <you@example.com>
2022-04-01 09:15:22 +0200

    Initial commit

--- src/main/java/com/example/DigitaleAnwesenheitsliste/Persons/MainController.java
+++ src/main/java/com/example/DigitaleAnwesenheitsliste/Persons/MainController.java
@@ -1,4 +1,4 @@
-package com.example.DigitaleAnwesenheitsliste.Controller;
+package com.example.DigitaleAnwesenheitsliste.Persons;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
